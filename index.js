c0400205d7fb23c9726686a43b4c6538c3588ab1
Valentin5944 <valek.prt2013@gmail.com>
2026-09-12 19:51:52 +0300

    Created feat/monitoring branch

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 const HOST = 'localhost';
 const PORT = 3000;
 
-
 app.get('/timestamp', (req, res) => {
     const currentDate = new Date();
 
@@ -14,7 +13,22 @@ app.get('/timestamp', (req, res) => {
     });
 });
 
+app.get('/health', (req, res) => {
+    res.json({
+        status: "ok"
+    });
+});
+
+app.get('/stats', (req, res) => {
+    res.json({
+        uptime: Math.floor(process.uptime()),
+        nodeVersion: process.version,
+        timestamp: new Date().toISOString()
+    });
+});
 
 app.listen(PORT, HOST, () => {
-    console.log(`server is running on http://localhost:3000/timestamp`);
+    console.log(`Server is running on http://${HOST}:${PORT}/timestamp`);
+    console.log(`Server is running on http://${HOST}:${PORT}/health`);
+    console.log(`Server is running on http://${HOST}:${PORT}/stats`);
 });
